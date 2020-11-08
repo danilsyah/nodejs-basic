de0d125d580cb9559a11f70eb6958f789b4e6824
danil syah <danilsyaharihardjo@gmail.com>
2020-11-08 23:50:57 +0700

    create module file

--- app.js
+++ app.js
@@ -2,8 +2,21 @@
 // fs.copyFileSync("text.txt", "hasil.txt");
 // console.log("source.txt was copied to destination.txt");
 
-const hero = require("superheroes");
+// const hero = require("superheroes");
 
-for (let i = 0; i < 10; i++) {
-  console.log(hero.random());
-}
+// for (let i = 0; i < 10; i++) {
+//   console.log(hero.random());
+// }
+
+// import modul buatan sendiri
+const modulOperator = require("./module-operator");
+
+const tambah = modulOperator.tambah(5, 3);
+const kurang = modulOperator.kurang(5, 3);
+const kali = modulOperator.kali(5, 3);
+const title = modulOperator.title;
+
+console.log(title);
+console.log(tambah);
+console.log(kurang);
+console.log(kali);
---
+++ module-operator.js
@@ -0,0 +1,13 @@
+exports.tambah = function (a, b) {
+  return a + b;
+};
+
+exports.kurang = function (a, b) {
+  return a - b;
+};
+
+exports.kali = function (a, b) {
+  return a * b;
+};
+
+exports.title = "Belajar Operator Aritmatika";
